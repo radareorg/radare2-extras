c90b07612507f4ffaa8260ddae11b3452514cbe9
pancake <pancake@nopcode.org>
2013-12-31 15:39:08 +0100

    Import x86im and x86simple plugins

---
+++ libr/anal/arch/x86/dislen/dislen.d
@@ -0,0 +1,3 @@
+/Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.o: \
+  /Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.c \
+  /Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.h
