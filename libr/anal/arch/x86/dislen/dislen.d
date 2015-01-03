6174ebf07a5a986dfb0bbb309e13a2052812e45e
Jody Frankowski <jody.frankowski@gmail.com>
2015-01-03 15:25:10 +0100

    Remove *.d files

--- libr/anal/arch/x86/dislen/dislen.d
+++
@@ -1,3 +0,0 @@
-/Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.o: \
-  /Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.c \
-  /Users/pancake/prg/radare2/libr/..//libr/anal/p/../arch/x86/dislen/dislen.h
