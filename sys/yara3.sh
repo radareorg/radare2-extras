ca656864a54deb26efbeb107704e1cdfbba4f3c3
pancake <pancake@nopcode.org>
2015-08-09 05:55:29 +0200

    Add sys/yara3.sh installation script

--- sys/yara3.sh
+++ sys/yara3.sh
@@ -7,9 +7,11 @@ fi
 SUDO=sudo
 cd yara/yara3
 ./configure --prefix=/usr || exit 1
+make clean
 make || exit 1
 if [ "`id -u`" = 0 ]; then
 	${SUDO} make install || exit 1
 else
+	echo "[yara3] installing at home"
 	cp -f core_yara3.dylib ~/.config/radare2/plugins
 fi
