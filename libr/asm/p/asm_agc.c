6fa7ca813b2d8a629f69b63a00a643440db9a644
ibabushkin <inokentiy.babushkin@googlemail.com>
2016-07-18 10:33:23 +0200

    [WIP] Apollo Guidance Computer disassembly (#54)
    * Implemented basic structure for AGC plugin.
    
    Implemented integration into build system and layout, as well as some
    barebones setup code. Now onto the actual logic :3
    
    * Added basic structure of AGC disassembler
    
    The core disassembly logic has been ported over from
    https://github.com/alfonsokim/apollo-guidance-computer/blob/master/yaAGC/agc_disassembler.c .
    However, since the disassembler is inherently stateful, we still need
    to implement either state or port over the whole lib / rewrite it.
    Or I get to find a different solution for this.
    
    * Done some research and gathered some information
    
    It seems clear how to get the current address etc. from the data
    passed to the plugin without resorting to internal state. That way
    a lot of complexity should be avoidable and implementation gets a
    lot easier.
    
    * Cleaned up logic
    
    * Integrated things
    
    * Finished integration of disasm plugin.
    
    * Worked through the AGC instruction set
    
    Gathered info needed for the anal plugin and commented the disassembler
    components.
    
    * Fixed compiler error
    
    * Added some constants and fixed errors
    
    * Added help file

---
+++ libr/asm/p/asm_agc.c
@@ -0,0 +1,36 @@
+#include <r_lib.h>
+#include <r_asm.h>
+#include "../arch/agc/asm_agc.h"
+
+static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
+    op->buf_asm[0] = 0;
+    disasm_instruction(a->pc,
+            ((const ut16 *)buf)[0], op->buf_asm, R_ASM_BUFSIZE);
+    op->size = 2;
+    return op->size;
+}
+
+static int assemble(RAsm *a, RAsmOp *op, const char *buf) {
+	// TODO
+	return 0;
+}
+
+RAsmPlugin r_asm_plugin_agc = {
+	.name = "agc",
+	.arch = "agc",
+	.bits = 16,
+    .license = "GPL3",
+	.desc = "AGC (Apollo Guidance Computer) disassembly plugin",
+	.init = NULL,
+	.fini = NULL,
+	.disassemble = &disassemble,
+	.assemble = &assemble
+};
+
+#ifndef CORELIB
+struct r_lib_struct_t radare_plugin = {
+	.type = R_LIB_TYPE_ASM,
+	.data = &r_asm_plugin_agc,
+	.version = R2_VERSION
+};
+#endif
