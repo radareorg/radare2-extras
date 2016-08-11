f705cc7893a9a54160200954006091bcd9811c93
Paul <pvt.gord@gmail.com>
2016-08-11 14:57:43 +0200

    ssdeep plugin is added (#62)

---
+++ ssdeep/ssdeep-lib/uninstall-ssdeep.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SSDEEP_VERSION="2.13"
+
+TMP_PATH=/var/tmp/
+REP_PATH=`pwd`
+
+MAKE=make
+WGET=wget
+SUDO=sudo
+
+cd ${TMP_PATH}
+${WGET} -O ssdeep.tar.gz https://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download
+tar zxvf ssdeep.tar.gz
+cd ssdeep-${SSDEEP_VERSION}/
+cp "${REP_PATH}/ssdeep/ssdeep-lib/ac-fix/*" ./
+./configure
+${MAKE}
+${SUDO} ${MAKE} uninstall
+cd ../
+rm -rf ssdeep-${SSDEEP_VERSION}
+rm ssdeep.tar.gz 
+
