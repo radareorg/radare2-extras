5925878aec17a99fb1ffa74649adf1e8b43ce23c
Sven Steinbauer <sven@unlogic.co.uk>
2016-10-22 22:14:01 +0100

    Add z80 package

---
+++ libr/asm/p/z80_nc.mk
@@ -0,0 +1,13 @@
+OBJ_Z80=../arch/z80_nc/z80.o \
+	../arch/z80_nc/disasm.o \
+	../arch/z80_nc/test.o \
+	../arch/z80_nc/z80asm.o \
+	asm_z80_nc.o
+
+STATIC_OBJ+=${OBJ_Z80}
+TARGET_Z80=asm_z80_nc.${LIBEXT}
+
+ALL_TARGETS+=${TARGET_Z80}
+
+${TARGET_Z80}: ${OBJ_Z80}
+	${CC} $(call libname,asm_z80_nc) ${CFLAGS} -o ${TARGET_Z80} ${OBJ_Z80}
