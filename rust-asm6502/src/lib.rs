e81b8647610bf171aad54538ded4725982fd9523
pancake <pancake@nopcode.org>
2017-01-03 11:48:14 +0100

    Use Optional function pointers and comment dead code

--- rust-asm6502/src/lib.rs
+++ rust-asm6502/src/lib.rs
@@ -4,6 +4,12 @@ extern crate disasm6502;
 use libc::*;
 use std::ffi::CString;
 
+const MY_NAME : *const c_char = b"6502.rs\0" as *const [u8] as *const c_char;
+const R2_VERSION: &'static [u8] = b"1.2.0-git\0";
+const MY_ARCH : &'static [u8] = b"6502\0";
+const MY_DESC : &'static [u8] = b"6502 disassembler in Rust\0";
+const MY_LICENSE : &'static [u8] = b"MIT\0";
+
 // order matters because of libr/util/lib.c
 #[repr(C,i32)]
 pub enum RLibType {
@@ -35,17 +41,13 @@ pub struct RAsmPlugin {
     user: usize,
     bits: c_int,
     endian: c_int,
-    // bool (*init)(void *user);
-    pub init: usize, //extern "C" fn(*mut c_void) -> bool,
-    // bool (*fini)(void *user);
-    pub fini: usize, //extern "C" fn(*mut c_void) -> bool,
-    // int (*disassemble)(RAsm *a, RAsmOp *op, const ut8 *buf, int len);
-    pub disassemble: extern "C" fn(*const c_void, *mut RAsmOp, *const uint8_t, c_int) -> c_int,
-    // int (*assemble)(RAsm *a, RAsmOp *op, const char *buf);
-    pub assemble: extern "C" fn(*const c_void, *const c_char) -> c_int,
-    pub modify: extern "C" fn(*mut c_void, *mut uint8_t, c_int, uint64_t) -> c_int,
-    pub set_subarch: extern "C" fn(*const c_void, *const c_char) -> c_int,
-    pub mnemonics: extern "C" fn(*const c_void, c_int, bool) -> *mut c_char,
+    pub init: Option<extern "C" fn(*mut c_void) -> bool>,
+    pub fini: Option<extern "C" fn(*mut c_void) -> bool>,
+    pub disassemble: Option<extern "C" fn(*const c_void, *mut RAsmOp, *const uint8_t, c_int) -> c_int>,
+    pub assemble: Option<extern "C" fn(*const c_void, *const c_char) -> c_int>,
+    pub modify: Option<extern "C" fn(*mut c_void, *mut uint8_t, c_int, uint64_t) -> c_int>,
+    pub set_subarch: Option<extern "C" fn(*const c_void, *const c_char) -> c_int>,
+    pub mnemonics: Option<extern "C" fn(*const c_void, c_int, bool) -> *mut c_char>,
     features: *const [u8]
 }
 
@@ -85,21 +87,6 @@ pub struct RLibStruct {
 	pub version: *const [u8]
 }
 
-// static MY_NAME:StaticCString = StaticCString(b"6502.rs\0" as *const u8);
-const R2_VERSION: &'static [u8] = b"1.2.0-git\0";
-const MY_NAME : &'static [u8] = b"6502.rs\0";
-const MY_ARCH : &'static [u8] = b"6502\0";
-const MY_DESC : &'static [u8] = b"6502 disassembler in Rust\0";
-const MY_LICENSE : &'static [u8] = b"MIT\0";
-
-extern "C" fn _init (foo: *mut c_void) -> bool {
-    true
-}
-
-extern "C" fn _mnemonics (foo: *const c_void, bar: c_int, cow: bool) -> *mut c_char {
-    b"\0".as_ptr() as *mut c_char
-}
-
 extern "C" fn _disassemble (asm: *const c_void, asmop: *mut RAsmOp, buf: *const u8, len: c_int) -> c_int {
     let oplen : usize = std::cmp::min(len, 3) as usize;
     unsafe {
@@ -115,7 +102,6 @@ extern "C" fn _disassemble (asm: *const c_void, asmop: *mut RAsmOp, buf: *const 
                 let opstr = ins.as_str();
                 let hexstr = ins.as_hex_str().replace(" ", "");
                 let inslen = (hexstr.len() / 2) as c_int;
-                println!("{}", inslen);
                 (*asmop).size = inslen;
                 (*asmop).payload = 0;
                 (*asmop).buf_asm[0] = 0;
@@ -139,6 +125,15 @@ extern "C" fn _disassemble (asm: *const c_void, asmop: *mut RAsmOp, buf: *const 
     -1 
 }
 
+/*
+extern "C" fn _init (foo: *mut c_void) -> bool {
+    true
+}
+
+extern "C" fn _mnemonics (foo: *const c_void, bar: c_int, cow: bool) -> *mut c_char {
+    b"\0".as_ptr() as *mut c_char
+}
+
 extern "C" fn _assemble (asm: *const c_void, str: *const c_char) -> c_int {
     0
 }
@@ -149,11 +144,10 @@ extern "C" fn _modify (asm: *mut c_void, buf: *mut uint8_t, len: c_int, at: uint
 extern "C" fn _set_subarch (asm: *const c_void, arch: *const c_char) -> c_int {
     0
 }
-
-const MYNAME : *const c_char = b"6502.rs\0" as *const [u8] as *const c_char;
+*/
 
 const r_asm_plugin_6502rs: RAsmPlugin = RAsmPlugin {
-    name : MYNAME,
+    name : MY_NAME,
     arch : MY_ARCH as *const [u8] as *const c_char,
     license : MY_LICENSE as *const [u8] as *const c_char,
     user : 0,
@@ -161,13 +155,13 @@ const r_asm_plugin_6502rs: RAsmPlugin = RAsmPlugin {
     desc : MY_DESC as *const [u8] as *const c_char,
     bits : 16,
     endian: 0,
-    disassemble: _disassemble,
-    assemble: _assemble,
-    init: 0,
-    fini: 0,
-    modify: _modify,
-    set_subarch: _set_subarch,
-    mnemonics: _mnemonics,
+    disassemble: Some(_disassemble),
+    assemble: None, //Some(_assemble),
+    init: None,
+    fini: None,
+    modify: None, //_modify,
+    set_subarch: None, //_set_subarch,
+    mnemonics: None, //_mnemonics,
     features: b"\0"
 };
 
@@ -176,9 +170,8 @@ const r_asm_plugin_6502rs: RAsmPlugin = RAsmPlugin {
 pub static mut radare_plugin: RLibStruct = RLibStruct {
     _type : RLibType::RLibTypeAsm,
     data : ((&r_asm_plugin_6502rs) as *const RAsmPlugin) as *const c_void,
-    version : b"1.2.0-git\0"
+    version : R2_VERSION
 };
-
 
 #[cfg(test)]
 mod tests {
