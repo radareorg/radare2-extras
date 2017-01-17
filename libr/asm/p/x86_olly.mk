92ebbf827db258b4a78380672919ac6951260c6f
Sven Steinbauer <sven@unlogic.co.uk>
2017-01-17 14:30:32 +0000

    Add x86_olly to extras (#94)

---
+++ libr/asm/p/x86_olly.mk
@@ -0,0 +1,12 @@
+OBJ_X86_OLLY=asm_x86_olly.o
+OBJ_X86_OLLY+=../arch/x86/ollyasm/disasm.o
+OBJ_X86_OLLY+=../arch/x86/ollyasm/asmserv.o
+OBJ_X86_OLLY+=../arch/x86/ollyasm/assembl.o
+
+STATIC_OBJ+=${OBJ_X86_OLLY}
+TARGET_X86_OLLY=asm_x86_olly.${LIBEXT}
+
+ALL_TARGETS+=${TARGET_X86_OLLY}
+
+${TARGET_X86_OLLY}: ${OBJ_X86_OLLY}
+	${CC} $(call libname,asm_x86_olly) ${LDFLAGS} ${CFLAGS} -o ${TARGET_X86_OLLY} ${OBJ_X86_OLLY}
