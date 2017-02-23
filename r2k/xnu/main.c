e078e85e3545760f46a5311266e31f901fdd4dd5
pancake <pancake@nopcode.org>
2017-02-23 12:38:52 +0100

    Add sample kext for XNU (macOS)

---
+++ r2k/xnu/main.c
@@ -0,0 +1,14 @@
+/* Sample r2k kext for XNU -- pancake@nopcode.org */
+
+#include <sys/systm.h>
+#include <mach/mach_types.h>
+ 
+kern_return_t r2k_start (kmod_info_t * ki, void * d) {
+    printf("r2k has started.\n");
+    return KERN_SUCCESS;
+}
+ 
+kern_return_t r2k_stop (kmod_info_t * ki, void * d) {
+    printf("r2k has stopped.\n");
+    return KERN_SUCCESS;
+}
