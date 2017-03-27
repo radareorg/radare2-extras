90354a536ce2399a11ae764e1791bb700636f108
l0stb1t <phanthanhduypr@gmail.com>
2017-03-27 16:47:05 +0700

    forgot to add new file so I fix it

---
+++ libr/asm/p/asm_pyc.c
@@ -0,0 +1,50 @@
+#include <r_types.h>
+#include <r_lib.h>
+#include <r_util.h>
+#include <r_asm.h>
+
+#include "pyc_dis.h"
+
+static int disassemble(RAsm *a, RAsmOp *opstruct, const ut8 *buf, int len) {
+	ut64 pc = a->pc;
+	RList *shared = NULL;
+	RList *interned_table = NULL;
+	RList *cobjs = NULL;
+	RBin *bin = a->binb.bin;
+	RBinPlugin *plugin = bin && bin->cur && bin->cur->o ?
+		bin->cur->o->plugin : NULL;
+
+	if (plugin) {
+		if (!strcmp (plugin->name, "pyc")) {
+			shared = bin->cur->o->bin_obj;
+		}
+	}
+	cobjs = r_list_get_n (shared, 0);
+	interned_table = r_list_get_n (shared, 1);
+	int r = r_pyc_disasm (opstruct, buf, cobjs, interned_table, pc);
+	opstruct->size = r;
+	return r;
+}
+
+static int init(void *user) {
+	init_opname_table ();
+	return 0;
+}
+
+RAsmPlugin r_asm_plugin_pyc = {
+	.name = "pyc",
+	.arch = "pyc",
+	.license = "LGPL3",
+	.bits = 32,
+	.desc = "PYC disassemble plugin",
+	.disassemble = &disassemble,
+	.init = &init,
+};
+
+#ifndef CORELIB
+struct r_lib_struct_t radare_plugin = {
+	.type = R_LIB_TYPE_ASM,
+	.data = &r_asm_plugin_pyc,
+	.version = R2_VERSION
+};
+#endif
