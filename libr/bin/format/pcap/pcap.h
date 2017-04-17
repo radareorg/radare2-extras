deaa4d55463ea3a818dce88278cf04d322a120db
Srimanta Barua <srimanta.barua1@gmail.com>
2017-04-17 16:29:57 +0530

    Initial pcap plugin (#116)
    * Loaded pcap file
    
    * Added info about network type
    
    * Initial loading of symbols at structures
    
    * Added strings
    
    * Separated into functions
    
    * Fixed tcp endianness issue
    
    * Fixed incorrect comment
    
    * Removed unneeded RList

---
+++ libr/bin/format/pcap/pcap.h
@@ -0,0 +1,122 @@
+#include <r_util.h>
+
+#ifndef _PCAP_H_
+#define _PCAP_H_
+
+// Definitions
+#define PCAP_MAGIC      0xa1b2c3d4 // Magic number for pcap files
+#define PCAP_NSEC_MAGIC 0xa1b23c4d // Modified pcap with nsec resolution
+#define LIBPCAP_MAGIC   0xa1b2cd34 // "libpcap" with Alexey Kuznetsoc's patches
+
+// The network field in the pcap file header
+typedef enum pcap_net {
+	NOLINK = 0,
+	ETHERNET = 1,
+	ETHERNET_3MB = 2,
+	AX_25 = 3,
+	PRONET = 4,
+	CHAOS = 5,
+	TOKEN_RING = 6,
+	ARCNET = 7,
+	SLIP = 8,
+	PPP = 9,
+	FDDI = 10,
+	RFC_1483_ATM_1 = 11,
+	RAW_IP_1 = 12,
+	BSDOS_SLIP_1 = 13,
+	BSDOS_PPP_1 = 14,
+	LINUX_ATM_CLASSICAL_IP = 19,
+	PPP_CISCO_HDLC = 50,
+	PPP_OVER_ETHERNET = 51,
+	SYMANTEC_FIREWALL = 99,
+	RFC_1483_ATM_2 = 100,
+	RAW_IP_2 = 101,
+	BSDOS_SLIP_2 = 102,
+	BSDOS_PPP_2 = 103,
+	BSDOS_CISCO_HDLC = 104,
+	_802_11 = 105,
+	LINUX_CLASSICAL_IP_ATM = 106,
+	FRAME_RELAY = 107,
+	OPENBSD_LOOPBACK = 108,
+	OPENBSD_IPSEC_ENC = 109,
+	CISCO_HDLC = 112,
+	LINUX_COOKED = 113,
+	LOCALTALK = 114,
+	OPENBSD_PFLOG = 117,
+	_802_11_PRISM = 119,
+	RFC_2625_IP_FIBRE_CHANNEL = 122,
+	SUNATM = 123,
+	_802_11_RADIOTAP = 127,
+	LINUX_ARCNET = 129,
+	APPLE_IP_IEEE_1394 = 138,
+	MTP2 = 140,
+	MTP3 = 141,
+	DOCSIS = 143,
+	IRDA = 144,
+	_802_11_AVS_HDR = 163,
+} pcap_net_t;
+
+// pcap file header
+typedef struct pcap_file_hdr {
+	ut32 magic;			// magic number
+	ut16 version_major;
+	ut16 version_minor;
+	int this_zone;		// GMT to local correction
+	ut32 ts_accuracy;	// Accuracy of timestamps
+	ut32 max_pkt_len;	// Max length of captured packets in bytes
+	pcap_net_t network;	// Data link type
+} pcap_file_hdr_t;
+
+// Packet record header, always 16 bytes
+typedef struct pcak_pktrec_hdr {
+	ut32 ts_sec;	// Timestamp in seconds
+	ut32 ts_usec;	// Timestamp in usec (nanosec for PCAP_NSEC_MAGIC)
+	ut32 cap_len;	// Length of packet captured
+	ut32 orig_len;	// Original length of packet
+} pcap_pktrec_hdr_t;
+
+// Ethernet header, always 14 bytes
+typedef struct pcap_pktrec_ether {
+	ut8  dst[6];	// Destination MAC address
+	ut8  src[6];	// Source MAC address
+	ut16 type;		// 0x0080 = IPV4, etc
+} pcap_pktrec_ether_t;
+
+// IPV4 header, atleast 20 bytes
+typedef struct pcap_pktrec_ipv4 {
+	ut8  ver_len;	// Upper nibble = version, lower = header len in 4-byte words
+	ut8  diff_serv;	// Differentiated services field
+	ut16 tot_len;	// Total length of IPV4 packet
+	ut16 id;
+	ut16 flag_frag;	// Upper 3 bits = flags, lower 13 = fragment offset
+	ut8  ttl;
+	ut8  protocol;	// 6 = TCP
+	ut16 chksum;
+	ut32 src;		// Source IP
+	ut32 dst;		// Destination IP
+} pcap_pktrec_ipv4_t;
+
+// TCP header, 20 - 60 bytes
+typedef struct pcap_pktrec_tcp {
+	ut16 src_port;	// Port on source
+	ut16 dst_port;	// Port on destination
+	ut32 seq_num;	// Sequence number
+	ut32 ack_num;	// Ack number
+	ut8  hdr_len;	// Length of TCP header
+	ut16 flags;		// TCP flags
+	ut16 win_sz;	// Window size
+	ut16 chksum;
+	ut16 urgnt_ptr;	// Urgent
+	// Variable length options. Use hdr_len
+} pcap_pktrec_tcp_t;
+
+
+void read_pcap_file_hdr(pcap_file_hdr_t *hdr, const ut8 *buf, int swap_endian);
+void read_pcap_pktrec_hdr(pcap_pktrec_hdr_t *hdr, const ut8 *buf, int swap_endian);
+void read_pcap_pktrec_ether(pcap_pktrec_ether_t *hdr, const ut8 *buf, int swap_endian);
+void read_pcap_pktrec_ipv4(pcap_pktrec_ipv4_t *hdr, const ut8 *buf, int swap_endian);
+void read_pcap_pktrec_tcp(pcap_pktrec_tcp_t *hdr, const ut8 *buf, int swap_endian);
+
+const char* pcap_net_type (ut32 net);
+
+#endif  // _PCAP_H_
---
+++ libr/bin/p/bin_pcap.c
@@ -0,0 +1,356 @@
+#include <r_bin.h>
+#include <r_lib.h>
+#include <r_types.h>
+#include "pcap.h"
+
+#define OPP_ENDIAN  1
+#define SAME_ENDIAN 0
+
+// The pcap object for RBinFile
+typedef struct pcap_obj {
+	struct pcap_file_hdr header;	// File header
+	bool is_nsec;					// nsec timestamp resolution?
+	int endian;	// Relative endianness (same or different from host)
+} pcap_obj_t;
+
+
+// Functions
+
+static RBinInfo *info(RBinFile *arch) {
+	if (!arch || !arch->o || !arch->o->bin_obj) {
+		return NULL;
+	}
+	RBinInfo *ret = R_NEW0 (RBinInfo);
+	if (!ret) {
+		return NULL;
+	}
+	pcap_file_hdr_t *header = &((pcap_obj_t *) arch->o->bin_obj)->header;
+	ret->file = strdup (arch->file);
+	ret->type = r_str_newf ("tcpdump capture file - version %d.%d (%s, "
+		"capture length %"PFMT32u ")", header->version_major,
+		header->version_minor, pcap_net_type (header->network),
+		header->max_pkt_len);
+	ret->rclass = strdup ("pcap");
+	return ret;
+}
+
+static bool check_bytes(const ut8 *buf, ut64 length) {
+	if (!buf || length < sizeof (pcap_file_hdr_t) || length == UT64_MAX) {
+		return false;
+	}
+	pcap_file_hdr_t *header = (pcap_file_hdr_t *) buf;
+	switch (header->magic) {
+	case PCAP_MAGIC:
+	case PCAP_NSEC_MAGIC:
+		return true;
+	}
+	switch (r_swap_ut32 (header->magic)) {
+	case PCAP_MAGIC:
+	case PCAP_NSEC_MAGIC:
+		return true;
+	}
+	return false;
+}
+
+static void *load_bytes(RBinFile *arch, const ut8 *buf, ut64 sz, ut64 loadaddr, Sdb *sdb) {
+	if (!buf || !sz || sz < sizeof (pcap_file_hdr_t) || sz == UT64_MAX) {
+		return NULL;
+	}
+	struct pcap_obj *obj = NULL;
+	if (!(obj = R_NEW0 (pcap_obj_t))) {
+		return NULL;
+	}
+	memcpy (&obj->header, buf, sizeof (pcap_file_hdr_t));
+
+	obj->endian = SAME_ENDIAN;
+	switch (obj->header.magic) {
+	case PCAP_MAGIC:
+		obj->is_nsec = false;
+		break;
+	case PCAP_NSEC_MAGIC:
+		obj->is_nsec = true;
+		break;
+	default:
+		switch (r_swap_ut32 (obj->header.magic)) {
+		case PCAP_MAGIC:
+			obj->is_nsec = false;
+			break;
+		case PCAP_NSEC_MAGIC:
+			obj->is_nsec = true;
+			break;
+		default:
+			free (obj);
+			return NULL;
+		}
+		obj->endian = OPP_ENDIAN;
+	}
+	// Reload file header
+	read_pcap_file_hdr (&obj->header, buf, obj->endian);
+	return obj;
+}
+
+static void _read_tcp_sym(RList *list, const ut8 *buf, ut64 off, ut64 sz, ut64 tcplen, int endian) {
+	RBinSymbol *ptr = NULL;
+	if (off + sizeof (pcap_pktrec_tcp_t) > sz) {
+		return;
+	}
+	if (!(ptr = R_NEW0 (RBinSymbol))) {
+		return;
+	}
+	pcap_pktrec_tcp_t tcp;
+	read_pcap_pktrec_tcp (&tcp, &buf[off], endian);
+	ut64 tcp_data_len = tcplen - (((tcp.hdr_len >> 4) & 0x0F) * 4);
+	ptr->name = r_str_newf ("0x%x: Transmission Control Protocol, Src Port: %d, Dst"
+		" port: %d, Len: %d", off, tcp.src_port, tcp.dst_port,
+		tcp_data_len);
+	ptr->paddr = ptr->vaddr = off;
+	r_list_append (list, ptr);
+}
+
+static void _read_ipv4_sym(RList *list, const ut8 *buf, ut64 off, ut64 sz, int endian) {
+	RBinSymbol *ptr = NULL;
+	if (!(ptr = R_NEW0 (RBinSymbol))) {
+		return;
+	}
+	pcap_pktrec_ipv4_t ipv4;
+	read_pcap_pktrec_ipv4 (&ipv4, &buf[off], endian);
+	ptr->name = r_str_newf ("0x%x: IPV%d, Src: %d.%d.%d.%d, Dst: %d.%d.%d.%d",
+		off, (ipv4.ver_len >> 4) & 0x0F, (ipv4.src >> 24) & 0xFF,
+		(ipv4.src >> 16) & 0xFF, (ipv4.src >> 8) & 0xFF,
+		ipv4.src && 0xFF, (ipv4.dst >> 24) & 0xFF,
+		(ipv4.dst >> 16) & 0xFF, (ipv4.dst >> 8) & 0xFF,
+		ipv4.dst && 0xFF);
+	ptr->paddr = ptr->vaddr = off;
+	r_list_append (list, ptr);
+	if (off + ipv4.tot_len > sz) {
+		return;
+	}
+	off += (ipv4.ver_len & 0x0F) * 4;
+
+	// For now, if not TCP, continue. TODO others
+	switch (ipv4.protocol) {
+	case 6:
+		_read_tcp_sym (list, buf, off, sz, (ipv4.tot_len - ((ipv4.ver_len & 0x0F) * 4)), endian);
+	}
+}
+
+static void _read_ether_sym(RList *list, const ut8 *buf, ut64 off, ut64 sz, int endian) {
+	RBinSymbol *ptr = NULL;
+	if (!(ptr = R_NEW0 (RBinSymbol))) {
+		return;
+	}
+	pcap_pktrec_ether_t ether;
+	read_pcap_pktrec_ether (&ether, &buf[off], endian);
+	ptr->name = r_str_newf ("0x%x: Ethernet, Src: %02"PFMT32x ":%02"PFMT32x ":%02"PFMT32x
+		":%02"PFMT32x ":%02"PFMT32x ":%02"PFMT32x ", Dst: %02"PFMT32x
+		":%02"PFMT32x ":%02"PFMT32x ":%02"PFMT32x ":%02"PFMT32x
+		":%02"PFMT32x, off, ether.src[0], ether.src[1],
+		ether.src[2], ether.src[3], ether.src[4], ether.src[5],
+		ether.dst[0], ether.dst[1], ether.dst[2], ether.dst[3],
+		ether.dst[4], ether.dst[5]);
+	ptr->paddr = ptr->vaddr = off;
+	r_list_append (list, ptr);
+	off += sizeof (pcap_pktrec_ether_t);
+
+	// For now, if not IPV4, continue. TODO IPV6
+	switch (ether.type) {
+	case 0x08:
+		_read_ipv4_sym (list, buf, off, sz, endian);
+	}
+}
+
+static RList *symbols(RBinFile *arch) {
+	RBinSymbol *ptr = NULL;
+	RList *ret = NULL;
+	pcap_obj_t *obj = NULL;
+	const ut8 *buf = NULL;
+	ut64 sz = 0;
+	ut64 off;
+	ut64 pkt_num = 0;
+	if (!arch || !arch->o || !arch->o->bin_obj || !arch->buf || !arch->buf->buf) {
+		return NULL;
+	}
+	obj = arch->o->bin_obj;
+	buf = r_buf_buffer (arch->buf);
+	sz = r_buf_size (arch->buf);
+	if (sz == 0 || sz == UT64_MAX) {
+		return NULL;
+	}
+	if (!(ret = r_list_new ())) {
+		return NULL;
+	}
+
+	// File header
+	if (!(ptr = R_NEW0 (RBinSymbol))) {
+		return ret;
+	}
+	ptr->name = r_str_newf ("tcpdump capture file - version %d.%d (%s, "
+		"capture length %"PFMT32u ")", obj->header.version_major,
+		obj->header.version_minor, pcap_net_type (obj->header.network),
+		obj->header.max_pkt_len);
+	ptr->paddr = ptr->vaddr = 0;
+	r_list_append (ret, ptr);
+
+	// Go through each packet
+	off = sizeof (pcap_file_hdr_t);
+	while (off <= sz - sizeof (pcap_pktrec_hdr_t)) {
+		pkt_num++;
+
+		// Frame header
+		if (!(ptr = R_NEW0 (RBinSymbol))) {
+			break;
+		}
+		pcap_pktrec_hdr_t pkthdr;
+		read_pcap_pktrec_hdr (&pkthdr, &buf[off], obj->endian);
+		ptr->name = r_str_newf ("0x%x: Frame %d, %d bytes on wire, %d bytes captured",
+			off, pkt_num, pkthdr.orig_len, pkthdr.cap_len);
+		ptr->paddr = ptr->vaddr = off;
+		r_list_append (ret, ptr);
+
+		// Check if rest of file is present. If not, break
+		if (off + sizeof (pcap_pktrec_hdr_t) + pkthdr.cap_len > sz) {
+			break;
+		}
+
+		// For now, if not ethernet, continue. TODO others
+		switch (obj->header.network) {
+		case ETHERNET:
+			_read_ether_sym (ret, buf, off + sizeof (pcap_pktrec_hdr_t), sz, obj->endian);
+		default:
+			break;
+		}
+		off += sizeof (pcap_pktrec_hdr_t) + pkthdr.cap_len;
+	}
+
+	return ret;
+}
+
+static RList *strings(RBinFile *arch) {
+	RBinString *ptr = NULL;
+	RList *ret = NULL;
+	pcap_obj_t *obj = NULL;
+	const ut8 *buf = NULL;
+	char *tmp = NULL;
+	ut64 sz = 0;
+	ut64 off;
+	if (!arch || !arch->o || !arch->o->bin_obj || !arch->buf || !arch->buf->buf) {
+		return NULL;
+	}
+	obj = arch->o->bin_obj;
+	buf = r_buf_buffer (arch->buf);
+	sz = r_buf_size (arch->buf);
+	if (sz == 0 || sz == UT64_MAX) {
+		return NULL;
+	}
+	if (!(ret = r_list_new ())) {
+		return NULL;
+	}
+	if (!(tmp = malloc (obj->header.max_pkt_len + 1))) {
+		return ret;
+	}
+
+	// Go through each packet
+	off = sizeof (pcap_file_hdr_t);
+	while (off <= sz - sizeof (pcap_pktrec_hdr_t)) {
+		ut64 tmp_off = off;
+		// Frame header
+		pcap_pktrec_hdr_t pkthdr;
+		read_pcap_pktrec_hdr (&pkthdr, &buf[tmp_off], obj->endian);
+		if (tmp_off + sizeof (pcap_pktrec_hdr_t) + pkthdr.cap_len > sz) {
+			break;
+		}
+		off += sizeof (pcap_pktrec_hdr_t) + pkthdr.cap_len;
+
+		// Ethernet data. For now, if not ethernet, continue. TODO others
+		if (obj->header.network != ETHERNET) {
+			continue;
+		}
+		tmp_off += sizeof (pcap_pktrec_hdr_t);
+		pcap_pktrec_ether_t ether;
+		read_pcap_pktrec_ether (&ether, &buf[tmp_off], obj->endian);
+
+		// IPV4 data. For now, if not IPV4, continue. TODO IPV6
+		if (ether.type != 0x08) {
+			continue;
+		}
+		tmp_off += sizeof (pcap_pktrec_ether_t);
+		pcap_pktrec_ipv4_t ipv4;
+		read_pcap_pktrec_ipv4 (&ipv4, &buf[tmp_off], obj->endian);
+		if (tmp_off + ipv4.tot_len > sz) {
+			continue;
+		}
+
+		// TCP header data. For now, if not TCP, continue. TODO others}
+		if (ipv4.protocol != 6) {
+			continue;
+		}
+		tmp_off += (ipv4.ver_len & 0x0F) * 4;
+		if (tmp_off + sizeof (pcap_pktrec_tcp_t) > sz) {
+			continue;
+		}
+		pcap_pktrec_tcp_t tcp;
+		read_pcap_pktrec_tcp (&tcp, &buf[tmp_off], obj->endian);
+		ut64 tcp_data_len = (ipv4.tot_len - ((ipv4.ver_len & 0x0F) * 4)) -
+				    (((tcp.hdr_len >> 4) & 0x0F) * 4);
+		if (tcp_data_len <= 1) {
+			continue;
+		}
+		tmp_off += ((tcp.hdr_len >> 4) & 0x0F) * 4;
+		if (tcp_data_len > obj->header.max_pkt_len || tmp_off + tcp_data_len > sz) {
+			continue;
+		}
+		size_t str_len;
+
+		memset (tmp, 0, obj->header.max_pkt_len);
+		strncpy (tmp, (const char *) &buf[tmp_off], tcp_data_len);
+		tmp[tcp_data_len] = '\0';
+		if (!(str_len = strlen (tmp))) {
+			continue;
+		}
+		if (!(ptr = R_NEW0 (RBinString))) {
+			break;
+		}
+		ptr->string = strdup (tmp);
+		ptr->paddr = ptr->vaddr = tmp_off;
+		ptr->length = str_len;
+		ptr->size = ptr->length + 1;
+		ptr->type = R_STRING_TYPE_DETECT;
+		r_list_append (ret, ptr);
+
+	}
+	free (tmp);
+	return ret;
+}
+
+static bool load(RBinFile *arch) {
+	if (!arch || !arch->o) {
+		return false;
+	}
+	const ut8 *bytes = r_buf_buffer (arch->buf);
+	ut64 size = r_buf_size (arch->buf);
+	if (!bytes || size == 0 || size == UT64_MAX) {
+		return false;
+	}
+	arch->o->bin_obj = load_bytes (arch, bytes, size, arch->o->loadaddr, arch->sdb);
+	return arch->o->bin_obj != NULL;
+}
+
+RBinPlugin r_bin_plugin_pcap = {
+	.name = "pcap",
+	.desc = "libpcap .pcap format r2 plugin",
+	.license = "LGPL3",
+	.info = info,
+	.load = load,
+	.strings = strings,
+	.symbols = symbols,
+	.load_bytes = load_bytes,
+	.check_bytes = check_bytes,
+};
+
+#ifndef CORELIB
+struct r_lib_struct_t radare_plugin = {
+	.type = R_LIB_TYPE_BIN,
+	.data = &r_bin_plugin_pcap,
+	.version = R2_VERSION,
+};
+#endif
