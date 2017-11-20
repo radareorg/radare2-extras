3c433863552fc0ea6fe9c7e73b31b218bcc03cbe
Fedor Sakharov <fedor.sakharov@gmail.com>
2017-11-20 23:40:31 +0300

    Improves EVM support.
    Adds anal, io and debug plugins.

---
+++ libr/anal/p/evm.mk
@@ -0,0 +1,10 @@
+OBJ_evm=anal_evm.o
+CFLAGS+=-I../asm/arch/evm/
+
+STATIC_OBJ+=${OBJ_evm}
+TARGET_evm=anal_evm.${LIBEXT}
+
+ALL_TARGETS+=${TARGET_evm}
+
+${TARGET_evm}: ${OBJ_evm} ${SHARED_OBJ}
+	${CC} $(call libname,anal_evm) ${LDFLAGS} ${CFLAGS} -o ${TARGET_evm} ${OBJ_evm}
