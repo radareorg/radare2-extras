60285a5370c158deffb32afb61c9ee22e23fd6c7
pancake <pancake@nopcode.org>
2018-03-14 22:34:04 +0100

    Initial import of the asm.x86.udis86 plugin
    Moved from radare2 (core)

---
+++ libr/asm/p/x86_udis.mk
@@ -0,0 +1,12 @@
+OBJ_X86_UDIS86=asm_x86_udis.o
+OBJ_X86_UDIS86+=../arch/x86/udis86/*.c
+
+STATIC_OBJ+=${OBJ_X86_UDIS86}
+TARGET_X86_UDIS86=asm_x86_udis.${LIBEXT}
+
+ALL_TARGETS+=${TARGET_X86_UDIS86}
+
+CFLAGS+=-I../arch/x86
+
+${TARGET_X86_UDIS86}: ${OBJ_X86_UDIS86}
+	${CC} $(call libname,asm_x86_udis) ${LDFLAGS} ${CFLAGS} -o ${TARGET_X86_UDIS86} ${OBJ_X86_UDIS86}
