5943bf373a90cf68eeb5330ad74fc4b16d8c61f6
ilovezfs <ilovezfs@icloud.com>
2018-04-11 14:08:03 +0000

    x86_udis.mk: also pass LDFLAGS

--- libr/anal/p/x86_udis.mk
+++ libr/anal/p/x86_udis.mk
@@ -19,5 +19,5 @@ ALL_TARGETS+=${TARGET_X86_UDIS86}
 CFLAGS+=-I../../asm/arch/x86 -I../../asm/arch/x86/udis86 -DHAVE_STRING_H=1
 
 ${TARGET_X86_UDIS86}: ${OBJ_X86_UDIS86} ${OBJ_ESIL_UDIS86}
-	${CC} ${CFLAGS} $(call libname,anal_x86) -lr_reg -lr_anal -lr_util \
+	${CC} ${CFLAGS} ${LDFLAGS} $(call libname,anal_x86) -lr_reg -lr_anal -lr_util \
 		$(SRC_X86_UDIS86) -o anal_x86_udis.${EXT_SO} ${OBJ_X86_UDIS86} ${OBJ_ESIL_UDIS86}
