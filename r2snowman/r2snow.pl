78336f49779cacf2f7867fb9701c77526d418ea6
pancake <pancake@nopcode.org>
2019-07-24 06:45:43 +0200

    More fixes for r2snow

--- r2snowman/r2snow.pl
+++ r2snowman/r2snow.pl
@@ -20,11 +20,26 @@ sub wf {
 }
 
 use constant {
-  R2SNOW_SOURCE => "r2snow-source.c"
+  R2SNOW_SOURCE => "r2snow-source.c",
+  R2SNOW_ADDROF => "r2snow-addrof.txt"
 };
 
+if ($arg == '-a') {
+  print($ts);
+}
+
+if ($arg == '-f') {
+my $off = $ENV{"R2_OFFSET"};
+print(functionAt(lineFor(scalar $off)));
+  exit();
+
+}
+if ($arg == '-r') {
+# do nothing, defaults
+}
+
 my $source = join("", rf(R2SNOW_SOURCE));
-my @addrof = rf("r2snow-addrof.txt");
+my @addrof = rf(R2SNOW_ADDROF);
 
 my $ts = $source;
 $ts =~s/reinterpret_cast//g;
@@ -44,11 +59,35 @@ sub initNewlines() {
   }
 }
 
+sub functionAt($) {
+  my ($ln) = @_;
+  my $line = 1;
+  my $findeof = 0;
+  my $text = "";
+  foreach my $nt (@nts) {
+    if ($nt=~/^ /) {
+      $text .= $nt . "\n";
+    } else {
+      if ($findeof) {
+        return $text . $nt . "\n";
+      } else {
+        $text = $nt . "\n";
+      }
+    }
+    if ($line >= $ln) {
+      $findeof = 1;
+    }
+    $line++;
+  }
+  return "";
+}
+
 sub lineFor($) {
   my ($offset) = @_;
   my $line = 0;
-  foreach my $nl (@nls) {
-    if ($nl >= $offset) {
+  foreach my $a (@addrof) {
+    my ($addr, $off) = split(" ", $a);
+    if (hex($addr) >= $offset) {
       return $line;
     }
     $line++;
@@ -58,13 +97,14 @@ sub lineFor($) {
 
 initNewlines();
 
+
 foreach my $a (@addrof) {
   my ($addr, $offset) = split(" ", $a);
   my $file = R2SNOW_SOURCE . ".txt";
   my $line = lineFor($offset);
   my $text = @nts[$line - 1];
-  my $txt64=encode_base64($text);
-  $txt64=~ s/\n//g;
+  my $txt64 = encode_base64($text);
+  $txt64 =~ s/\n//g;
   print "CCu base64:$txt64 @ $addr\n";
   #print "CL $file:$line $addr\n";
 }
