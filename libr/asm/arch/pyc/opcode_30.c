369ca8a56ee8904a10ebe771d6f25de0eabac4ce
FXTi <zjxiang1998@gmail.com>
2020-04-16 22:00:39 +0800

    Fix

--- libr/asm/arch/pyc/opcode_30.c
+++ libr/asm/arch/pyc/opcode_30.c
@@ -24,8 +24,8 @@ pyc_opcodes *opcode_30 () {
 	def_op (.op_obj = ret->opcodes, .op_name = "SET_ADD", .op_code = 17, .pop = 1, .push = 0);
 	def_op (.op_obj = ret->opcodes, .op_name = "LIST_APPEND", .op_code = 18, .pop = 2, .push = 1);
 
-	jrel_op (.op_obj = ret->opcodes, .op_name = "JUMP_IF_FALSE", .op_code = 111, .pop = 1, .push = 1);
-	jrel_op (.op_obj = ret->opcodes, .op_name = "JUMP_IF_TRUE", .op_code = 112, .pop = 1, .push = 1);
+	jrel_op (.op_obj = ret->opcodes, .op_name = "JUMP_IF_FALSE", .op_code = 111, .pop = 1, .push = 1, .conditional = true);
+	jrel_op (.op_obj = ret->opcodes, .op_name = "JUMP_IF_TRUE", .op_code = 112, .pop = 1, .push = 1, .conditional = true);
 
 	r_list_purge (ret->opcode_arg_fmt);
 	add_arg_fmt (ret, "EXTENDED_ARG", format_extended_arg);
