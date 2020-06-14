dc97e4a92861cfb143ffbea40d933e877280d23c
Jerome-PS <Jerome-PS>
2020-06-14 17:38:36 +0200

    Adding support for BA2 CPU

---
+++ libr/asm/p/ba2.mk
@@ -0,0 +1,10 @@
+OBJ_BA2=asm_ba2.o
+CFLAGS+=-I./arch/ba2/
+
+STATIC_OBJ+=${OBJ_BA2}
+TARGET_BA2=asm_ba2.${LIBEXT}
+
+ALL_TARGETS+=${TARGET_BA2}
+
+${TARGET_BA2}: ${OBJ_BA2}
+	${CC} ${CFLAGS} -o ${TARGET_BA2} ${OBJ_BA2}
