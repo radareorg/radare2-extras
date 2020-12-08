3bd587018078617378d4a9e6c5879f4e717d928c
pancake <pancake@nopcode.org>
2020-12-08 13:18:46 +0100

    One more fix

--- libr/asm/p/arm_v35.mk
+++ libr/asm/p/arm_v35.mk
@@ -3,7 +3,7 @@
 OBJ_ARMV35=asm_arm_v35.o
 ARM64V35_HOME=$(shell pwd)/../arch/arm/v35arm64/
 include ../arch/arm/v35arm64/deps.mk
-OBJ_ARMV35+=$(addprefix ../arch/arm/v35arm64/disassembler/,$(ARM64V35_OBJS))
+OBJ_ARMV35+=$(ARM64V35_LINK)
 
 STATIC_OBJ+=${OBJ_ARMV35}
 SHARED_OBJ+=${SHARED_ARMV35}
