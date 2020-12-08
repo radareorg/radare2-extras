3d4494ceae7b5473cb9d66b402e214a315849a20
pancake <pancake@nopcode.org>
2020-12-08 13:49:10 +0100

    One more tweak

--- libr/asm/arch/arm/v35arm64/deps.mk
+++ libr/asm/arch/arm/v35arm64/deps.mk
@@ -7,7 +7,8 @@ ARM64V35_OBJS=arm64dis.o pcode.o decode0.o decode1.o decode_fields32.o decode_sc
 ARM64V35_LINK=$(addprefix $(ARM64V35_SRCDIR),$(ARM64V35_OBJS))
 
 ${ARM64V35_LINK}: $(ARM64V35_SRCDIR)
-$(ARM64V35_SRCDIR): git-clone-arm64v35
+$(ARM64V35_SRCDIR): 
+	$(MAKE) git-clone-arm64v35
 
 git-clone-arm64v35:
 	$(MAKE) -C $(ARM64V35_HOME) all
