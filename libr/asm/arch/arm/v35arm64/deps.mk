c62549a09c38b57cb6e8491fcada300601a19411
pancake <pancake@nopcode.org>
2020-12-08 13:32:48 +0100

    Phony dat

--- libr/asm/arch/arm/v35arm64/deps.mk
+++ libr/asm/arch/arm/v35arm64/deps.mk
@@ -7,7 +7,10 @@ ARM64V35_CFLAGS=-I$(ARM64V35_SRCDIR)
 ARM64V35_OBJS=arm64dis.o pcode.o decode0.o decode1.o decode_fields32.o decode_scratchpad.o decode2.o operations.o encodings.o sysregs.o
 ARM64V35_LINK=$(addprefix $(ARM64V35_SRCDIR),$(ARM64V35_OBJS))
 
-${ARM64V35_LINK}: git-clone-arm64v35
+${ARM64V35_LINK}: $(ARM64V35_SRCDIR)
+$(ARM64V35_SRCDIR): git-clone-arm64v35
 
 git-clone-arm64v35:
 	$(MAKE) -C $(ARM64V35_HOME) all
+
+.PHONY: git-clone-arm64v35
