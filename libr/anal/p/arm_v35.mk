3b97be5a8738042d8876d3456e0680054efcf0d3
pancake <pancake@nopcode.org>
2020-12-08 12:44:48 +0100

    Fix absolute path builds

--- libr/anal/p/arm_v35.mk
+++ libr/anal/p/arm_v35.mk
@@ -1,5 +1,5 @@
 N=anal_arm_v35
-ARM64V35_HOME=../../asm/arch/arm/v35arm64/disassembler
+ARM64V35_HOME=$(shell pwd)/../../asm/arch/arm/v35arm64/
 include ../../asm/arch/arm/v35arm64/deps.mk
 OBJ_ARM_V35=anal_arm_v35.o
 OBJ_ARM_V35+=${ARM64V35_LINK}
@@ -10,7 +10,7 @@ TARGET_ARM_V35=$(N).${LIBEXT}
 
 ALL_TARGETS+=${TARGET_ARM_V35}
 
-${TARGET_ARM_V35}: ${OBJ_ARM_V35}
+${TARGET_ARM_V35}: $(OBJ_ARM_V35)
 	${CC} ${CFLAGS} $(call libname,$(N)) $(V35_CFLAGS) \
 		-lr_util -lr_search \
 		-o $(TARGET_ARM_V35) ${OBJ_ARM_V35} $(V35_LDFLAGS)
