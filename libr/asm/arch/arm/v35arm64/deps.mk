32152f2ec26266af34449ac57c7fc509306420e3
pancake <pancake@nopcode.org>
2021-04-27 12:26:05 +0200

    Fix arm64v35 build

--- libr/asm/arch/arm/v35arm64/deps.mk
+++ libr/asm/arch/arm/v35arm64/deps.mk
@@ -3,7 +3,21 @@ ARM64V35_HOME?=arch-arm64/
 ARM64V35_SRCDIR=$(ARM64V35_HOME)/disassembler/
 
 ARM64V35_CFLAGS=-I$(ARM64V35_SRCDIR)
-ARM64V35_OBJS=format.o pcode.o decode.o decode0.o decode1.o decode_fields32.o decode_scratchpad.o decode2.o operations.o encodings_dec.o regs.o
+ARM64V35_OBJS+=decode.o
+ARM64V35_OBJS+=decode0.o
+ARM64V35_OBJS+=decode1.o
+ARM64V35_OBJS+=decode2.o
+ARM64V35_OBJS+=decode_fields32.o
+ARM64V35_OBJS+=decode_scratchpad.o
+ARM64V35_OBJS+=encodings_dec.o
+ARM64V35_OBJS+=encodings_fmt.o
+ARM64V35_OBJS+=format.o
+ARM64V35_OBJS+=gofer.o
+ARM64V35_OBJS+=operations.o
+ARM64V35_OBJS+=pcode.o
+ARM64V35_OBJS+=regs.o
+ARM64V35_OBJS+=sysregs.o
+#ARM64V35_OBJS+=test.o
 ARM64V35_LINK=$(addprefix $(ARM64V35_SRCDIR),$(ARM64V35_OBJS))
 
 ${ARM64V35_LINK}: $(ARM64V35_SRCDIR)
