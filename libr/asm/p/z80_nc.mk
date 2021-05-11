d4daecfcedbb783d8fd2099ff2cf9c68860d7d4e
pancake <pancake@nopcode.org>
2021-05-11 18:00:58 +0200

    Fix build of z80.nc

--- libr/asm/p/z80_nc.mk
+++ libr/asm/p/z80_nc.mk
@@ -9,4 +9,4 @@ TARGET_Z80=asm_z80_nc.${LIBEXT}
 ALL_TARGETS+=${TARGET_Z80}
 
 ${TARGET_Z80}: ${OBJ_Z80}
-	${CC} $(call libname,asm_z80_nc) ${CFLAGS} -o ${TARGET_Z80} ${OBJ_Z80}
+	${CC} $(call libname,asm_z80_nc) ${CFLAGS} -o ${TARGET_Z80} ${OBJ_Z80} -lr_util
