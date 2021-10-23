e8c9895354312ae2ba44e11080deb6f49174a158
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2021-10-24 00:19:48 +0200

    Add separate CFLAGS to vc4 compilation (#299)

--- libr/asm/p/vc4.mk
+++ libr/asm/p/vc4.mk
@@ -14,11 +14,21 @@ OBJ_VC4+=../arch/vc4/libvc4/vc4_decode.o
 OBJ_VC4+=../arch/vc4/libvc4/vc4_arch.o
 OBJ_VC4+=../arch/vc4/libvc4/vc4_util.o
 
-CFLAGS+=-Iarch/vc4/libvc4/ -Iarch/include/
+SRC_VC4=$(patsubst %.o, %.c, $(OBJ_VC4))
+
+CFLAGS_VC4=$(R2_CFLAGS)
+CFLAGS_VC4+=-g -I$(TOP)/shlr
+CFLAGS_VC4+=-Wall -shared -fPIC ${LDFLAGS_LIB}
+
+CFLAGS_VC4+=-I../arch/vc4/libvc4/ -I../arch/vc4/include/ -I../arch/vc4/include/opcode/
+
 STATIC_OBJ+=${OBJ_VC4}
 TARGET_VC4=asm_vc4.${LIBEXT}
 
 ALL_TARGETS+=${TARGET_VC4}
 
+$(OBJ_VC4): %.o : %.c
+	$(CC) -c $(CFLAGS_VC4) $< -o $@
+
 ${TARGET_VC4}: ${OBJ_VC4}
-	${CC} $(call libname,asm_vc4) ${LDFLAGS} ${CFLAGS} -o asm_vc4.${LIBEXT} ${OBJ_VC4}
+	${CC} $(call libname,asm_vc4) ${LDFLAGS} ${CFLAGS_VC4} -o asm_vc4.${LIBEXT} ${OBJ_VC4}
--- libr/asm/p/x86_zyan.mk
+++ libr/asm/p/x86_zyan.mk
@@ -1,3 +1,8 @@
+CFLAGS_ZYAN=$(R2_CFLAGS)
+CFLAGS_ZYAN+=-g -I$(TOP)/shlr -I../arch/ -I../arch/include
+CFLAGS_ZYAN+=-Wall -shared -fPIC ${LDFLAGS_LIB}
+CFLAGS_ZYAN+=-I../arch/x86/zyan/include
+
 OBJ_X86_ZYAN=asm_x86_zyan.o
 OBJ_X86_ZYAN+=../arch/x86/zyan/src/Decoder.o
 OBJ_X86_ZYAN+=../arch/x86/zyan/src/Formatter.o
@@ -9,9 +14,10 @@ OBJ_X86_ZYAN+=../arch/x86/zyan/src/Zydis.o
 
 STATIC_OBJ+=${OBJ_X86_ZYAN}
 TARGET_X86_ZYAN=asm_x86_zyan.$(LIBEXT)
-
 ALL_TARGETS+=${TARGET_X86_ZYAN}
-CFLAGS+=-I../arch/x86/zyan/include
+
+$(OBJ_X86_ZYAN): %.o : %.c
+	$(CC) -c $(CFLAGS_ZYAN) $< -o $@
 
 ${TARGET_X86_ZYAN}: ${OBJ_X86_ZYAN}
-	${CC} ${LDFLAGS} ${CFLAGS} -o ${TARGET_X86_ZYAN} ${OBJ_X86_ZYAN}
+	${CC} ${LDFLAGS} ${CFLAGS_ZYAN} -o ${TARGET_X86_ZYAN} ${OBJ_X86_ZYAN}
