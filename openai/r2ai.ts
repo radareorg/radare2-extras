8fe16da993991f6322841319a5c40e34c6e32111
pancake <pancake@nopcode.org>
2022-12-06 16:41:02 +0100

    Add the daila question

--- openai/r2ai.ts
+++ openai/r2ai.ts
@@ -8,15 +8,29 @@ const configuration = new Configuration({
     organization: "org-ges0Q9PvVCq5zJEpqwVgM6YC",
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+type QuestionType = "identify" | "readcode";
+
+function getQuestion(q: QuestionType) : string {
+	switch (q) {
+	case "identify":
+		return "What open source project is this code from. Please only give me the program name and package name:";
+	case "readcode":
+		return "Can you explain what this decompiled function do?";
+	}
+}
+
 async function main() {
 	const openai = new OpenAIApi(configuration);
+	try {
 	const response = await openai.listEngines();
 	var r2 = r2pipe.open()
-	const input = "Can you explain what this decompiled function do?\n```c\n" + r2.cmd("af;pdc") + "\n```\n";
+	const input = getQuestion("readcode");
 	const completion = await openai.createCompletion({
-		model: "text-davinci-002",
-		prompt: input,
+		model: "text-davinci-003",
+		prompt: input + "```c\n" + r2.cmd("af;pdg") + "\n```\n",
 	});
+	console.log(completion.data.choices);
 	const text = completion.data.choices[0].text;
 	if (useTTS) {
 		const filtered = text.replace(/[\n\*"]/, '');
@@ -24,6 +38,9 @@ async function main() {
 		r2.cmd("\"!say " + text + "\"");
 	} else {
 		console.log(text);
+	}
+	} catch (e) {
+		console.log("Set your OPENAI API with the %OPENAI_API_KEY env var");
 	}
 }
 main().then(function() {}).catch(function(err) {
