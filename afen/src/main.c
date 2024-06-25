6e4ece27320eb9ca9bd507fa5b0b3260b928dc70
satk0 <54475808+satk0@users.noreply.github.com>
2024-06-25 16:07:32 +0000

    Fix rparse to rcore binding for afen (#346)

--- afen/src/main.c
+++ afen/src/main.c
@@ -8,13 +8,12 @@ static RList *old_names;
 static RList *new_names;
 
 // afen parser
-static int r_afen_parse(RParse *p, const char *data, char *str) {
-	int res = true;
+static int r_parse_afen(RParse *p, const char *data, char *str) {
 	char *input = strdup (data);
 
-	int n = r_list_length(old_names);
+	int n = r_list_length (old_names);
 
-	if (n){
+	if (n) {
 		RListIter *s_iter_old = NULL;
 		RListIter *s_iter_new = NULL;
 
@@ -24,54 +23,63 @@ static int r_afen_parse(RParse *p, const char *data, char *str) {
 		for (int i = 0; i < n; ++i) {
 			input = r_str_replace_all (input, s_iter_old->data, s_iter_new->data);
 
-			s_iter_old = r_list_iter_get_next(s_iter_old);
-			s_iter_new = r_list_iter_get_next(s_iter_new);
+			s_iter_old = r_list_iter_get_next (s_iter_old);
+			s_iter_new = r_list_iter_get_next (s_iter_new);
 		}
 
 	}
 
 	strcpy (str, input);
-	return res;
+	return true;
 }
 
+// RParse plugin Definition Info
+RParsePlugin r_parse_plugin_afen = {
+	.name = "afen",
+	.desc = "Afen parse plugin",
+	.parse = r_parse_afen,
+};
+
 // sets afen parser
-static int r_cmd_init(void *user, const char *input) {
+static int r_core_init_afen(void *user, const char *input) {
 	RCmd *rcmd = (RCmd *) user;
 	RCore *core = (RCore *) rcmd->data;
+
+	r_parse_plugin_add (core->parser, &r_parse_plugin_afen);
+
 	old_names = r_list_new ();
 	new_names = r_list_new ();
-
-	core->parser->cur->parse = r_afen_parse;
 	
 	return true;
 }
 
-static int r_cmd_fini(void *user, const char *input) {
-	r_list_free(old_names);
-	r_list_free(new_names);
+static int r_core_fini_afen(void *user, const char *input) {
+	r_list_free (old_names);
+	r_list_free (new_names);
 
 	return true;
 }
 
-static int r_cmd_afen_client(void *user, const char *input) {
+static int r_core_call_afen(void *user, const char *input) {
 	if (r_str_startswith (input, "afen")) {
-		int *argc = (int*) malloc(sizeof(int));
-		char **argv = r_str_argv(input, argc);
+		int *argc = (int*) malloc (sizeof (int));
+		char **argv = r_str_argv (input, argc);
 
 		if (*argc != 3) {
-			r_cons_printf("Usage: afen new_name old_name\n");
+			r_cons_printf ("Usage: afen new_name old_name\n");
 			return true;
 		}
 
-		r_list_append(new_names, argv[1]);
-		r_list_append(old_names, argv[2]);
+		r_list_append (new_names, argv[1]);
+		r_list_append (old_names, argv[2]);
 
 		return true;
 	}
 	return false;
 }
 
-// PLUGIN Definition Info
+
+// RCore plugin Definition Info
 RCorePlugin r_core_plugin_afen = {
 	.meta = {
 		.name = "core-afen",
@@ -79,9 +87,9 @@ RCorePlugin r_core_plugin_afen = {
 		.author = "satk0",
 		.license = "GPLv3",
 	},
-	.call = r_cmd_afen_client,
-	.init = r_cmd_init,
-	.fini = r_cmd_fini
+	.call = r_core_call_afen,
+	.init = r_core_init_afen,
+	.fini = r_core_fini_afen
 };
 
 
