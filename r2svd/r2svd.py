9c22aa709df3665ea62d8e3b5d5cd04ab98a5f57
pancake <pancake@nopcode.org>
2024-09-29 19:23:14 +0200

    Parse register fields and support ascii art stuff for r2svd

--- r2svd/r2svd.py
+++ r2svd/r2svd.py
@@ -3,6 +3,7 @@
 # --pancake @ 2020-2023
 
 import json
+import base64
 import sys
 import math
 import os.path
@@ -60,20 +61,24 @@ else:
 supports_call = False
 try:
 	import r2pipe
-	r2 = r2pipe.open("-")
+	r2 = r2pipe.open("malloc://1")
 	supports_call = int(r2.cmd("?Vn")) >= 50800
 except Exception as e:
 	pass
 
 def print_flag(name, size, addr):
 	if supports_call:
-		print("\"\"f %s %d 0x%x"%(filter_name(name), size, addr))
+		print("'f %s %d 0x%x"%(filter_name(name), size, addr))
 	else:
 		print("f %s %d 0x%x"%(filter_name(name), size, addr))
 
+def print_bitfield(addr, size, fmt):
+	if supports_call:
+		print("'@0x%x'Cr %d pfb %s"%(addr,size,fmt))
+
 def print_comment(msg, addr):
 	if supports_call:
-		print("\"\"@0x%x\"\"CC %s"%(addr, filter_message(msg)))
+		print("'@0x%x'CC %s"%(addr, filter_message(msg)))
 	else:
 		print("CC %s @ 0x%x"%(filter_name(msg), addr))
 
@@ -85,16 +90,38 @@ for p in svd_dict['peripherals']:
 	except:
 		size = 4
 	print_comment(p['description'], addr)
-	print_flag(p['name'], size, addr)
+	pname = p['name']
+	print_flag(f"peripheral.{pname}", size, addr)
 	s = ""
+	cmt = []
+	bfsize = 0
 	for r in p['registers']:
+		fmt = ""
+		nam = ""
+		bfsize = r['size'] / 8
+		rname = r['name']
+		if 'description' in r:
+			c = r['description']
+			cmt.append(f"{pname}.{rname}: {c}")
 		offs = int(r['address_offset'])
-		at = int(addr) + math.floor(offs / 8)
+		at = int(addr) + offs # math.floor(offs / 8)
 		bt = (offs % 8)
-		s += " " + r['name']
 		if at != addr:
-			fname = "%s.%s"%(p["name"], r["name"])
+			fname = "reg.%s.%s"%(pname, rname)
 			print_flag(fname, size, at)
-	print_comment(s, addr)
+			if len(cmt) > 0:
+				c = base64.b64encode(bytes("\n".join(cmt), "utf-8"))
+				print_comment("base64:" + c.decode("utf-8"), at)
+				cmt = []
+		for f in r['fields']:
+			fname = f['name']
+			of = f['bit_offset']
+			wi = f['bit_width']
+			nam = f"{nam} {fname}"
+			fmt = f"{fmt}{wi}b"
+			# TODO. bit offset ignored. we are assuming fields are sorted properly
+		print_bitfield(at, bfsize, f"{fmt}{nam}")
+		fmt=""
+		nam=""
 
 # print(json.dumps(svd_dict, sort_keys=True, indent=4, separators=(',', ': ')))
